6b87fe62ffea7fb37fb72acb46370eac4a684488
bergtr <bern_gtr@outlook.com>
2020-06-21 12:06:25 +0700

    added /register and /signin endpoint

--- server.js
+++ server.js
@@ -1,9 +1,53 @@
 const express = require('express');
 
+
 const app = express();
+app.use(express.json());
+
+const database = {
+    users: [{
+            id: '123',
+            name: 'anton',
+            email: 'anton@cok.com',
+            password: 'jancok123',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'andi',
+            email: 'andi@cok.com',
+            password: 'kontol123',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
 
 app.get('/', (req, res) => {
-    res.send('bisacong')
+    res.send(database.users)
+})
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+        res.send('success')
+    } else {
+        res.status(400).json('error login')
+    }
+})
+
+app.post('/register', (req, res) => {
+    const { email, password, name } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length - 1]);
 })
 
 app.listen(3000, () => {
